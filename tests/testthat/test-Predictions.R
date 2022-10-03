cbdf6b952e7eadf38077709bd8f9f0694e1e0a72
Cristian Román-Palacios <cromanpa94@gmail.com>
2022-10-02 21:17:17 -0700

    Updating test

--- tests/testthat/test-Predictions.R
+++ tests/testthat/test-Predictions.R
@@ -3,7 +3,9 @@ ds <- sim.clumped(error = "S1", nobs = 50)
 paramdist <- simulateLM_measured(data = ds, replicates = 10)
 recData <- data.frame(Sample = c("Sample 1", "Sample 2"),
                       D47 = c(0.6, 0.7),
-                      D47error = c(0.005, 0.005))
+                      D47error = c(0.005, 0.005),
+                      n = c(2,2),
+                      Material = c(1,1))
 
 
 test_that("Number of samples is correct", {
