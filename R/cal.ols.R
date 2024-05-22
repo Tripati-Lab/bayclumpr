1b487e54b797fb805502d9a75876266a5166110c
Cristian Román-Palacios <cromanpa94@gmail.com>
2024-05-22 14:59:54 -0700

    handle errors in ols-based cals

--- R/cal.ols.R
+++ R/cal.ols.R
@@ -11,10 +11,12 @@
 cal.ols <- function(data, replicates, samples = NULL) {
   if(is.null(samples)){samples = nrow(data)}
   reps <- lapply(1:replicates, function(x) {
+    tryCatch({
     dataSub <- data[sample(seq_along(data[, 1]), nrow(data), replace = TRUE), ]
     Reg <- summary(lm(D47 ~ Temperature, dataSub))
     res <- cbind.data.frame("alpha" = Reg$coefficients[1, 1], "beta" = Reg$coefficients[2, 1])
     return(res)
+    }, error=function(e){})
   })
   reps <- do.call(rbind, reps)
   return(reps)
